e448d65c0625848b90a4c272ef9fa649c063339a
Arman173 <armanhaz173@gmail.com>
2022-03-29 21:10:12 -0500

    pipe mayuscula personalizado

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
 
 
 @NgModule({
@@ -13,7 +16,8 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
     NumerosComponent,
     NoComunesComponent,
     BasicosComponent,
-    OrdenarComponent
+    OrdenarComponent,
+    MayusculasPipe
   ],
   exports: [
     NumerosComponent,
